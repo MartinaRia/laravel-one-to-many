cd4885bcd50bfe55b8a7fc1e7888a8c6d2255589
Martina <martina.ria@hotmail.com>
2020-06-23 11:00:58 +0200

    Task List ok

--- one-to-many-project/resources/views/home.blade.php
+++ one-to-many-project/resources/views/home.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.template')
 
 @section('header-title')
-  <h1>Tutti gli Omini</h1>
+  <h1>Tasks</h1>
 @endsection
 
 @section('main-content')
     <div class="container-home flex-container">
       <div class="omini-container">
-        @foreach ($allEmployees as $employee)
+        @foreach ($allTasks as $task)
         <div class="omini-list">
-          <a href="" class="flex-container">
-            <h4>NAME: </h4>
-            <p>{{ $employee['firstname'] }}</p>
-            <h4>SURNAME: </h4>
-            <p>{{ $employee['lastname'] }}</p>
+          <a href="" class="">
+            <h4>{{ $task['name'] }}: </h4>
+            <p>{{ $task['description'] }}</p>
+            <h5>Employee assigned: {{ $task['employee']['firstname'] }} {{ $task['employee']['lastname'] }}</h5>
+            <br>
           </a>
         </div>
       @endforeach
@@ -22,7 +22,7 @@
       <div class="form-store">
         <h3>ADD NEW OMINO</h3>
         <br>
-        
+
       </div>
     </div> {{-- fine conatiner-home  --}}
 
